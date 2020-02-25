f5a7d3c42d7a6dd1faa42beca70646696d39b875
Sudarshan Giri <41857483+SudarshanGiri@users.noreply.github.com>
2020-02-25 13:46:13 +0545

    heroku deleted

--- ChatApp/settings.py
+++ ChatApp/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
-import django_heroku
+
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -132,8 +132,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles')
 MEDIA_URL= '/media/'
 
-# Activate Django-Heroku.
-django_heroku.settings(locals())
 # Configure Django App for Heroku.
     # import django_heroku
     # django_heroku.settings(locals())
