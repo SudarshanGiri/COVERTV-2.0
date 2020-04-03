ea30d3563847bcecdc4e1a2dd0a0f95045b6c098
Sudarshan Giri <41857483+SudarshanGiri@users.noreply.github.com>
2020-04-03 19:19:13 +0545

    virtual env uploaded to git

---
+++ myenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Sudarshan\Desktop\dprojects\covertv\covertv\myenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
